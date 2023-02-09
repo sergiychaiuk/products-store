5e49c2288cd81952750250ea519630fb2b6cdeca
sergiychaiuk <c2VyZ2l5Y2hhaXVr@gmail.com>
2023-02-09 18:26:47 +0200

    configured the nuxt.config file and created the main.css file

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - products-store',
-    title: 'products-store',
+    title: 'ProductsStore',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -24,6 +23,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/main.css',
+    '@sweetalert2/theme-material-ui',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -45,7 +46,15 @@ export default {
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    'nuxt-webfontloader',
+    'vue-sweetalert2/nuxt/no-css',
   ],
+
+  webfontloader: {
+    google: {
+      families: ['DM+Sans:wght@400;500;700&display=swap'],
+    },
+  },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
@@ -60,20 +69,40 @@ export default {
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
+    defaultAssets: {
+      font: {
+        family: "DM Sans",
+        size: 15,
+      },
+    },
+    treeShake: true,
     theme: {
-      dark: true,
+      light: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
+        light: {
+          primary: colors.deepPurple.darken1,
+          bg: "#ffffff",
+          surface: "#ffffff",
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+          success: colors.green.accent3,
+        },
+        dark: {
+          primary: colors.deepPurple.darken1,
+          bg: "#0a0514",
+          surface: "#130a24",
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
