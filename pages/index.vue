7ef418dfed35968a05c5f44da8d9958f5842e18b
sergiychaiuk <c2VyZ2l5Y2hhaXVr@gmail.com>
2023-02-10 17:15:20 +0200

    added the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,31 @@
 <template>
   <div>
     <Nav />
+    <HomeCarousel :sale_items="sale_items" />
+    <br /><br />
+    <v-container>
+      <h1 class="text-md-h4 text-h6">Check these out</h1>
+      <br />
+      <ProductSlider :products="products" />
+      <br /><br /><br />
+      <Newsletter />
+    </v-container>
     <Footer />
+    <ScrollTop />
   </div>
 </template>
+
+<script>
+export default {
+  async created() {
+    this.sale_items = await this.$content('product').where({ onSale: true }).fetch()
+    this.products = await this.$content('product').fetch()
+  },
+  data() {
+    return {
+      products: null,
+      sale_items: null,
+    }
+  },
+};
+</script>
